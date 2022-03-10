aab587f9b7aa3800f2fa775628859523df351a32
dbc2201 <divyanshb22@gmail.com>
2022-03-10 10:28:53 +0530

    complete the method

--- src/main/java/HandshakeCalculator.java
+++ src/main/java/HandshakeCalculator.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Signal> signals = new ArrayList<>();
+        if ((number & 1) == 1) {
+            signals.add(Signal.WINK);
+        }
+        if ((number & 2) == 2) {
+            signals.add(Signal.DOUBLE_BLINK);
+        }
+        if ((number & 4) == 4) {
+            signals.add(Signal.CLOSE_YOUR_EYES);
+        }
+        if ((number & 8) == 8) {
+            signals.add(Signal.JUMP);
+        }
+        if ((number & 16) == 16) {
+            Collections.reverse(signals);
+        }
+        return signals;
     }
 
 }
